37a5304add95beb6e8a1e9708c9f3c1d79848577
Sengimu <119593267+Sengimu@users.noreply.github.com>
2023-02-07 01:07:52 +0800

    Delete src directory

--- src/main/java/com/mirrorcraft/superfurnace/group/ModGroup.java
+++
@@ -1,7 +0,0 @@
-package com.mirrorcraft.superfurnace.group;
-
-import net.minecraft.world.item.CreativeModeTab;
-
-public class ModGroup {
-    public static final CreativeModeTab group = new SuperFurnaceGroup();
-}
